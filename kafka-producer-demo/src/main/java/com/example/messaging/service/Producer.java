46d3877dcc2f2a34e52b505259b66de187f23f8b
Wanderlei Souza <wandi.darko@gmail.com>
2023-12-05 11:45:35 -0300

    example log for traccing ID

--- kafka-producer-demo/src/main/java/com/example/messaging/service/Producer.java
+++ kafka-producer-demo/src/main/java/com/example/messaging/service/Producer.java
@@ -3,6 +3,7 @@ package com.example.messaging.service;
 import com.example.messaging.model.FactDTO;
 import com.github.javafaker.Faker;
 import io.micrometer.observation.annotation.Observed;
+import io.micrometer.tracing.Tracer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.NewTopic;
@@ -12,6 +13,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.Objects;
 
 @Slf4j
 @Service
@@ -21,6 +23,7 @@ public class Producer {
 
     private final KafkaTemplate<Integer, FactDTO> template;
     private final NewTopic factTopic;
+    private final Tracer tracer;
 
     @Scheduled(fixedRate = 1_000)
     public void produce() {
@@ -31,9 +34,12 @@ public class Producer {
         var record = new ProducerRecord<>(factTopic.name(), factTopic.numPartitions(), timestamp, fact.id(), fact);
 
         var futureResult = template.send(record);
-        futureResult.whenComplete((sr, ex) ->
-            log.info("Sent at {} timestamp: {}", timestamp, sr.getProducerRecord().value()));
 
+        //just and example how to get traceId from context
+        var traceId = Objects.requireNonNull(tracer.currentSpan()).context().traceId();
+        futureResult.whenComplete((sr, ex) ->
+            log.info("Producer sent message with traceId={} and timestamp={}, record={}",
+                traceId, timestamp, sr.getProducerRecord().value()));
     }
 
 }
